7dc4c638b8ecec9100d89bff0366410a7274a61e
aboudekahil <aboudeh003@gmail.com>
2023-09-22 23:20:19 +0300

    wtf

--- src/components/BmiResult.jsx
+++ src/components/BmiResult.jsx
@@ -1,16 +1,16 @@
 export default function BmiResult({result}){
     if(result < 0) return <h1></h1>;
 
-    let resultValue;
+    let resultValue = `Result: ${result.toFixed(2)} | `;
 
     if(result < 18.5){
-        resultValue = "Under weight";
+        resultValue += "Under weight";
     }else if(result <= 24.999){
-        resultValue = "Normal weight";
+        resultValue += "Normal weight";
     }else if(result <= 29.999){
-        resultValue = "Over weight";
+        resultValue += "Over weight";
     }else{
-        resultValue = "Obese";
+        resultValue += "Obese";
     }
 
     return <h1>{resultValue}</h1>
